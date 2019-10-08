3169aa65f18d1f9d3475d181876bb1e63f6f06e3
RyotoKuroki <kurohoge@gmail.com>
2019-10-09 01:35:27 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     devServer: {
-        disableHostCheck: true
+        public: 'strack-wanted-site.azurewebsites.net',
+        host: '0.0.0.0',
+        port: '8080'
     }
 }
